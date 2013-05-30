0165a4736b4a781997e5c6e3b17b88c01801cf0b
Gregory Bityukov <kamapcuc@gmail.com>
2013-05-30 16:01:04 +0400

    2

--- ru/combo_breaker/Main.java
+++ ru/combo_breaker/Main.java
@@ -8,7 +8,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
 
-
     public static void main(String[] args) throws IOException {
         AtomicBoolean tmp = new AtomicBoolean(false);
 
